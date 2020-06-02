0efb4b0aab53b72bcf3b7cd987ecf4dade87ac4e
uglynora <mangos8@yahoo.com>
2020-06-01 23:06:22 -0400

    Modified orbit-list.html - added table.

--- assignment6/src/app/satellite.ts
+++ assignment6/src/app/satellite.ts
@@ -4,7 +4,7 @@ orbitType: string;
 type: string;
 operational: boolean;
 launchDate: string;
-sourceList: Satellite[];
+//sourceList: object[];
 
 constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name = name;
@@ -12,13 +12,7 @@ constructor(name: string, type: string, launchDate: string, orbitType: string, o
     this.launchDate = launchDate;
     this.orbitType = orbitType;
     this.operational = operational;
-    this.sourceList = [
-        new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-        new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-        new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-        new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-        new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-     ];
+   
 
 }
 
