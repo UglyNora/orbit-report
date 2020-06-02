59cf6eb3a0b810cf173ae9eed0118810e56227bf
uglynora <mangos8@yahoo.com>
2020-06-02 18:50:39 -0400

    Modified app.component.ts

--- assignment6/src/app/app.component.ts
+++ assignment6/src/app/app.component.ts
@@ -12,13 +12,23 @@ export class AppComponent {
   title = 'assignment6';
   sourceList: Satellite[];
   constructor() {
-    this.sourceList = [
-       new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
-       new Satellite("Cat Scanner", "Imaging", "2012-01-05", "LOW", true),
-       new Satellite("Weber Grill", "Space Debris", "1996-03-25", "HIGH", false),
-       new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
-       new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
-    ];
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          // TODO: loop over satellites
+          for (let satellite in fetchedSatellites){
+          // TODO: create a Satellite object using 
+          let satObj = new Satellite(fetchedSatellites[satellite].name, fetchedSatellites[satellite].type, fetchedSatellites[satellite].launchDate, fetchedSatellites[satellite].orbitType, fetchedSatellites[satellite].operational);
+          // TODO: add the new Satellite object to sourceList using: 
+          this.sourceList.push(satObj);
+          }
+       }.bind(this));
+    }.bind(this));
+ 
  }
   
 }
