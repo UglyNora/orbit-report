533e47dbcb0d6d442b4da99a835e2cdabba726b8
uglynora <mangos8@yahoo.com>
2020-06-04 17:30:03 -0400

    Modified orbit-count component.ts and .html.

--- assignment6/src/app/orbit-count/orbit-count.component.ts
+++ assignment6/src/app/orbit-count/orbit-count.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
 
 @Component({
   selector: 'app-orbit-count',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orbit-count.component.css']
 })
 export class OrbitCountComponent implements OnInit {
-
+  @Input() satellites: Satellite[];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  countType(type: string): number {
+    let count = 0;
+    if(this.satellites){
+      for(let satellite of this.satellites){
+        if(satellite['type'].toLowerCase() === type.toLowerCase()){
+          count += 1;
+        }
+      }
+    }
+    return count;
   }
 
 }
